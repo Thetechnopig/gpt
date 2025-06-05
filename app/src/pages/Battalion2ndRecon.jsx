386baceddc2888da82423ddddc902e3f8c1e15af
Thetechnopig <indesweb@gmail.com>
2025-06-05 18:04:28 -0400

    Add battalions dropdown with subpages

---
+++ app/src/pages/Battalion2ndRecon.jsx
@@ -0,0 +1,8 @@
+export default function Battalion2ndRecon() {
+  return (
+    <div>
+      <h1>2nd Recon Battalion</h1>
+      <p>Information about the 2nd Recon Battalion.</p>
+    </div>
+  );
+}
